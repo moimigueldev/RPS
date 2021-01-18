c498328bed1d75178a92595862a1bb0f4f6d02f4
moises <moimigueldev@gmail.com>
2021-01-17 21:03:54 -0600

    completed the game

--- main.go
+++ main.go
@@ -4,12 +4,16 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+
+	"github.com/inancgumus/screen"
 )
 
 var choices = [3]string{"rock", "paper", "scissors"}
 
+var play = true
+
 type stats struct {
-	wins, losts, draws int
+	wins, loses, draws int
 }
 
 var (
@@ -20,16 +24,24 @@ var (
 var gameStats = stats{}
 
 func main() {
-	getUserChoice()
-	setCMPChoice()
-	compare()
 
+	for play {
+
+		getUserChoice()
+		setCMPChoice()
+		compare()
+		printStats()
+		playAgain()
+
+	}
 }
 
 func setCMPChoice() string {
 	x1 := rand.NewSource(time.Now().UnixNano())
 	y1 := rand.New(x1)
 	cmp = choices[y1.Intn(3)]
+
+	fmt.Printf("\n\nComputer Chose: %v", cmp)
 
 	return cmp
 }
@@ -56,26 +68,60 @@ func getUserChoice() string {
 func compare() {
 
 	if user == "rock" && cmp == "paper" {
-		fmt.Println("You Lost")
-		gameStats.losts++
+		fmt.Printf("\n\n~YOU LOST~\n\n")
+		gameStats.loses++
 	} else if user == "paper" && cmp == "rock" {
-		fmt.Println("You Won")
+		fmt.Printf("\n\n~YOU WON~\n\n")
 		gameStats.wins++
 	} else if user == "scissors" && cmp == "paper" {
-		fmt.Println("You Won")
+		fmt.Printf("\n\n~YOU WON~\n\n")
 		gameStats.wins++
 	} else if user == "paper" && cmp == "scissors" {
-		fmt.Println("You Lost")
-		gameStats.losts++
+		fmt.Printf("\n\n~YOU LOST~\n\n")
+		gameStats.loses++
 	} else if user == "rock" && cmp == "scissors" {
-		fmt.Println("You Won")
+		fmt.Printf("\n\n~You Won~\n\n")
 		gameStats.wins++
 	} else if user == "scissors" && cmp == "rock" {
-		fmt.Println("You Lost")
-		gameStats.losts++
+		fmt.Printf("\n\n~YOU LOST~\n\n")
+		gameStats.loses++
 	} else {
-		fmt.Println("You tied")
+		fmt.Printf("\n\n~YOU TIED~\n\n")
 		gameStats.draws++
 	}
+}
 
+func playAgain() {
+	input := "tempt"
+	fmt.Printf("Do You want To Play Again? (1) Yes (2) No > ")
+	fmt.Scanln(&input)
+	switch input {
+	case "2":
+		play = false
+		clearTheScreen()
+		printStats()
+	default:
+		play = true
+		clearTheScreen()
+	}
+
+}
+
+func clearTheScreen() {
+	screen.Clear()
+	screen.MoveTopLeft()
+	// Animate the time always in the same position
+	fmt.Println(time.Now())
+	i := 200
+	time.Sleep(time.Duration(i) * time.Millisecond)
+}
+
+func printStats() {
+
+	if play {
+		fmt.Printf("\n| WINS: %v | DRAWS: %v  | lOSES: %v |\n\n", gameStats.wins, gameStats.draws, gameStats.loses)
+	} else {
+		fmt.Printf("\nTHANKS FOR PLAYING, HERE ARE YOUR STATS: ")
+		fmt.Printf("\n\n| WINS: %v | DRAWS: %v  | lOSES: %v |\n\n", gameStats.wins, gameStats.draws, gameStats.loses)
+	}
 }
